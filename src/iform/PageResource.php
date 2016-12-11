707213dad2c1311898de0de82461a59117c5fc4f
Booker Bennett <bookerb@gmail.com>
2016-12-10 19:39:04 -0800

    Start PageResource wrapper

---
+++ src/iform/PageResource.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @author bookerb
+ * @package iform
+ */
+
+namespace iform;
+
+use IForm;
+
+
+class PageResource extends IForm\Resource
+{
+    /**
+     * UserResource constructor.
+     * @param $token
+     * @param $client_name
+     * @param $client_profile
+     */
+    public function __construct($token, $client_name, $client_profile)
+    {
+        $this->token = $token;
+        $this->httpurl = 'https://' . $client_name . '.iformbuilder.com/exzact/api/v60/profiles/self/pages';
+    }
+
+    public function getPageList($params)
+    {
+        $this->setParamArray($params);
+        return $this->getResponse('GET', $this->httpurl);
+    }
+
+}
